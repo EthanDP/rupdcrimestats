c82b781fb23a1b218307c810dac447e2868bf7ea
Ethan Peck <etdpeck@gmail.com>
2022-09-24 17:40:51 -0500

    styling stuff

--- rupdstats/rupdstats/settings.py
+++ rupdstats/rupdstats/settings.py
@@ -119,9 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "rupdheatmap", "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
