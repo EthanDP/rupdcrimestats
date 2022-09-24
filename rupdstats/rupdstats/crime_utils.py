3672ca78fa1404ac4612e30147fdea2960bf38ad
daniel <dan.li2010@hotmail.com>
2022-09-24 15:53:52 -0500

    implement basic crime stat functions

---
+++ rupdstats/rupdstats/crime_utils.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+class CrimeUtils:
+    def __init__(self) -> None:
+        pass
+    def find_unique_locations(self, df):
+        return np.unique(df['location'])
+
+    def find_most_common_crime(self, df):
+        crime_counts_zip = list(zip(df['crime_type'].value_counts().sort_index(ascending=True), np.unique(df['crime_type'])))
+        crime_counts_zip.sort(key = lambda x: x[0])
+        crime_type_ascending = [i[1] for i in crime_counts_zip]
+        return crime_type_ascending[-1]
+    
+    def find_most_dangerous_place(self, df):
+        grouped = df.groupby(['location']).size().reset_index()
+        print(grouped)
+        return grouped.iloc[grouped[0].idxmax()]['location']
+    
+    def find_crimes_per_day(self, df):
+        days = (df.iloc[0]['time_reported'] - df.iloc[-1]['time_reported']).days
+        return round(df.shape[0] / days, 2)
+
+    def find_crimes_per_week(self, df):
+        return round(self.find_crimes_per_day(df) * 7, 1)
+
+    def crimes_by_location(self, df, location):
+        grouped = df.groupby(['location']).size().reset_index()
+        grouped.columns = ["location", "count"]
+        return grouped.loc[grouped["location"] == location, 'count'].iloc[0]
---
+++ rupdstats/rupdstats/data_handler.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import html
+from crime_utils import CrimeUtils
+
+class CrimeDataParser:
+
+    def __init__(self):
+        pass
+
+    def get_crime_data_html(self, url):
+        crime_log_request = requests.get(url)
+        return BeautifulSoup(crime_log_request.text, 'lxml')
+    
+    def create_df(self, crime_html_data):
+        case_number = [td.text for td in crime_html_data.find_all('td',{'class':'case_number'})]
+        crime_type = [td.text for td in crime_html_data.find_all('td',{'class':'classification'})]
+        location = [html.unescape(td.text) for td in crime_html_data.find_all('td',{'class':'location'})]
+        time_reported = [td.text for td in crime_html_data.find_all('td',{'class':'datetime_reported'})]
+        status = [td.text for td in crime_html_data.find_all('td',{'class':'disposition'})]
+
+        data = {
+            'case_number': case_number,
+            'crime_type': crime_type,
+            'location': location,
+            'time_reported': time_reported
+        }
+
+        df = pd.DataFrame(data = data, columns = data.keys())
+
+        time_format_replacements = {
+            'a.m.':'AM',
+            'p.m.':'PM',
+            'noon':'12:00 PM',
+            'midnight':'12:00 AM'
+        }
+
+        df = df[df['time_reported'].apply(lambda x: len(x) > 18)]
+        df['time_reported'] = df['time_reported'].replace(time_format_replacements, regex=True)
+
+        df['time_reported'] = pd.to_datetime(df['time_reported'], format='%m/%d/%Y %I:%M %p')
+
+        return df
+    
+if __name__ == '__main__':
+    CRIME_DATA_URL = 'https://rupdadmin.rice.edu/crimelog/unskinned/'
+    cp = CrimeDataParser()
+    cdp = CrimeUtils()
+    data_html = cp.get_crime_data_html(CRIME_DATA_URL)
+    df = cp.create_df(data_html)
+    print(cdp.find_most_common_crime(df))
+    print(cdp.find_most_dangerous_place(df))
+    print(cdp.find_crimes_per_day(df))
+    print(cdp.find_crimes_per_week(df))
+    print(cdp.crimes_by_location(df, "Duncan College"))
