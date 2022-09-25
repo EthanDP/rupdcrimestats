91a621dce44114f833a733fbb5094057be99bddf
Ethan Peck <etdpeck@gmail.com>
2022-09-25 00:02:48 -0500

    Map setup, styling updates

--- rupdstats/rupdheatmap/views.py
+++ rupdstats/rupdheatmap/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    context = {}
+    context = {
+        
+    }
+    return render(request, "rupdheatmap/index.html", context)
+
+def filter_index(request, filter):
+    print(filter)
+    context = {
+        
+    }
     return render(request, "rupdheatmap/index.html", context)
